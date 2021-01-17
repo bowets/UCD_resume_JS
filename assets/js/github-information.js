5491db1d3c8a0ddff8e75636e43f65e3d6c88e2a
bowets <bforsek@gmail.com>
2021-01-17 17:57:59 +0000

    Fix bugs for user information displaying on Github page

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -16,11 +16,11 @@ function userInformationHTML(user) {
 }
 
 function repoInformationHTML(repos) {
-    if(repos.lenght == 0) {
+    if (repos.lenght == 0) {
         return `<div class="clearfix repo-list">No repos!</div>`
     }
 
-    var listItemsHTML = repos.map(function(repo) {
+    var listItemsHTML = repos.map(function (repo) {
         return `<li>
             <a href="${repo.html_url}" target="_blank">${repo.name}</a>
             </li>`
@@ -38,6 +38,9 @@ function repoInformationHTML(repos) {
 
 
 function fetchGitHubInformation(event) {
+
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
 
     var username = $("#gh-username").val();
     if (!username) {
@@ -70,3 +73,5 @@ function fetchGitHubInformation(event) {
         }
     );
 }
+
+$(document).ready(fetchGitHubInformation);
